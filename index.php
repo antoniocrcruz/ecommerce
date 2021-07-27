2796c1bf618c47b1bd3c3e6127ce491f9d5b7906
Antonio Cruz <fmcacrc2011@gmail.com>
2021-07-26 22:02:32 -0300

    Criando PageAdmin e TplAdmin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \antoniocrcruz\Page;
+use \antoniocrcruz\PageAdmin;
 
 $app = new Slim();
 
@@ -12,6 +13,14 @@ $app->config('debug', true);
 $app->get('/', function() {
     
 	$page = new Page(); //chama o header
+	
+	$page->setTpl("index"); //adiciona a página do corpo, após ele limpa a memória e carrega o destruct/arquivo footer.html
+
+});
+
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin(); //chama o header
 	
 	$page->setTpl("index"); //adiciona a página do corpo, após ele limpa a memória e carrega o destruct/arquivo footer.html
 
